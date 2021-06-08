5774201861825677c23792ed1797612c097a2133
rasengan <realrasengan@gmail.com>
2021-06-08 00:20:47 +0000

    Anonbot

---
+++ README.md
@@ -0,0 +1,13 @@
+# Anonchan
+### v0.01 ALPHA
+
+See #anonchan
+
+
+
+## Copyright and License
+Copyright (c) 2021 freenode limited
+All Rights Reserved
+
+Licensed under the MIT License
+
---
+++ bot.js
@@ -0,0 +1,78 @@
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
+const fs = require('fs');
+const Help = require('./lib/help.js');  // help text
+const constants = require('./lib/constants.js');  // constants and settings
+const IRC = new (require('./lib/irc.js')).IRC();  // irc client, connected
+
+// Hack
+// TODO: Better way to ensure one instance of bot only (pid file method breaks if process exits unexpectedly)
+var app = require('express')();
+app.listen(22534);
+
+// Nickslist - Stored in RAM, no logs, no leaks.
+let nicklist = [];
+
+// Main listener
+IRC.addListener('raw',async (message) => {
+  if(message.command==='PRIVMSG' &&
+    (message.args[0].toLowerCase()==='@'+constants.IRC_CHAN.toLowerCase()))
+    parse(message.nick,message.args[1]);
+  else if(message.command==='JOIN')
+    IRC.whois(message.nick);
+  else if(message.command==='PART')
+    delete nicklist[message.nick.toLowerCase()];
+  else if(message.command==='330') {
+    if(message.args[1].toLowerCase()!==message.args[2].toLowerCase()) {
+      IRC.mode(constants.IRC_CHAN,'+b',message.args[1]+"!*@*");
+      IRC.remove(constants.IRC_CHAN,message.args[1],"Sorry, but only primary nicks may join this channel.");
+      setTimeout(() => {
+        IRC.mode(constants.IRC_CHAN,'-b',message.args[1]+"!*@*");
+      },10000);
+    }
+    else {
+      nicklist[message.args[1].toLowerCase()]=getNick();
+      IRC.notice_chan(message.args[1],"Your nick is now "+nicklist[message.args[1].toLowerCase()],constants.IRC_CHAN);
+    }
+  }
+});
+
+// Main bot processor
+async function parse(from,msg) {
+  msg=msg.split(" ");
+  switch(msg[0].toLowerCase()) {
+    case "!help":
+      if(!msg[1])
+        IRC.notice_chan(from,Help.help,constants.IRC_CHAN);
+      else {
+        IRC.notice_chan(from,Help.header,constants.IRC_CHAN);
+        switch(msg[1].toLowerCase()) {
+          case '!new':
+            IRC.notice_chan(from,Help.help_new,constants.IRC_CHAN);
+            break;
+          default:
+            IRC.notice_chan(from,Help.none+msg[1],constants.IRC_CHAN);
+            break;
+        }
+        IRC.notice_chan(from,Help.footer,constants.IRC_CHAN);
+      }
+      break;
+    case '!new':
+      nicklist[from]=getNick();
+      IRC.notice_chan(from,"Your nick is now "+nicklist[from],constants.IRC_CHAN);
+      break;
+    default:
+      if(typeof nicklist[from] === 'undefined') {
+        nicklist[from]=getNick();
+        IRC.notice_chan(from,"Your nick is now "+nicklist[from],constants.IRC_CHAN);
+      }
+      IRC.say(constants.IRC_CHAN,"<"+nicklist[from]+"> "+msg.join(" "));
+      break;
+  }
+};
+
+// Random nick gen
+function getNick() {
+  let nick = uniqueNamesGenerator({ dictionaries: [animals] });
+  let color = uniqueNamesGenerator({ dictionaries: [colors] });
+  return color+nick+Math.floor(Math.random() * 9);
+}
